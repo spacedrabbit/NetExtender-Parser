b4549d0205484916c186ced6bd2c883a7472f56a
Louis Tur <louis.tur@gmail.com>
2014-05-10 18:20:38 -0400

    Basic Save Window open
    Nothing really new, just need to sync latest version to other laptop

--- NetExtender Parser/AppDelegate.m
+++ NetExtender Parser/AppDelegate.m
@@ -13,9 +13,12 @@
 
 @property (weak, nonatomic) IBOutlet NSButton * browseButton;
 @property (weak, nonatomic) IBOutlet NSTextField * filePathText;
+@property (strong, nonatomic) NSURL * filePath;
+@property (weak, nonatomic) NSScrollView * dataView;
+@property (unsafe_unretained) IBOutlet NSTextView *dataTextView;
 
 - (IBAction)browseButton:(NSButton *)sender;
-
+- (void)loadDataView;
 
 @end
 
@@ -35,9 +38,28 @@
     [panel setCanChooseDirectories:YES];
     [panel setCanChooseFiles:YES];
     
-    NSURL * path = [panel URL];
-    
-    //self.filePathText textDidChange:<#(NSNotification *)#>
-    
+    [panel beginWithCompletionHandler:^(NSInteger result){
+        
+        if(result == NSFileHandlingPanelOKButton){
+            
+            self.filePath = [panel URL];
+            
+            [self.filePathText setStringValue:[NSString stringWithFormat:@"%@",[self.filePath path]]];
+            
+            [self loadDataView];
+            
+        }
+        else{
+            NSLog(@"Guess you really didn't want that file, jerk");
+        }
+    }];
+
 }
+
+-(void)loadDataView{
+    NSString * data = [NSString stringWithContentsOfURL:self.filePath encoding:NSUTF8StringEncoding error:nil];
+    
+    [self.dataTextView setString:data];
+}
+
 @end
